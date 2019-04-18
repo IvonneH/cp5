8a3ca82950a09b71ceb78281dc05524dd199fdf0
Ivonne Hilton <hiltonivonne@gmail.com>
2019-04-17 22:54:42 -0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div id="left">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <div id="right">
+        <router-link to="/login">Login</router-link>
+        <router-link to="/register">Register</router-link>
+      </div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: gray;
+}
+#nav {
+  padding: 30px;
+  background-color: white;
+
+}
+
+#nav #left {
+  //text-align: left;
+  float: left;
+}
+
+#nav #right {
+  //text-align: right;
+  float: right;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 5px;
+}
+
+#nav a.router-link-exact-active {
+  color: green;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Welcome to this website!</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: gray;
+}
+</style>
